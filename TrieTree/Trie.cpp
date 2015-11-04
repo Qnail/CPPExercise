eb1337f7d73228489cca0e4374eef2f2f3cb078d
Qnail <qmeng2014@gmail.com>
2015-11-04 16:43:43 +0800

    version0.1

--- TrieTree/Trie.cpp
+++ TrieTree/Trie.cpp
@@ -6,36 +6,6 @@ using namespace std;
 
 int main()
 {
-	vector<string> files;
-	string s;
-	string sval;
 
-	for(int i = 1;i<3;++i)
-	{
-		cin>>sval;
-		files.push_back(sval);
-	}
-
-	vector<string>::const_iterator it = files.begin();
-	ifstream input;
-	while(it != files.end())
-	{
-		input.open(it->c_str());
-		if(!input)
-		{
-			cerr<<"error"<<*it<<"error-end"<<endl;
-			input.clear();
-			++it;
-		}else{
-			cout<<"else"<<endl;
-			input>>s;
-			while(input>>s)
-			{
-				cout<<"ok"<<endl;
-			}
-			input.close();
-			input.clear();
-			++it;sfd
-		}
-	}
+	return 0;
 }
---
+++ bloomFilter/bloomFilter.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s = "12";
+	cout<<s.size()<<" "<<sizeof(s)<<endl;
+	return 0;
+}
+
